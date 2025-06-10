1793935feef1548da187492f0447f3ba0ed80b3d
Bourkies <115338150+Bourkies@users.noreply.github.com>
2025-06-10 20:59:58 +1000

    Update Run_regex_test.bat
    Corrected the script

--- src/regex_test/Run_regex_test.bat
+++ src/regex_test/Run_regex_test.bat
@@ -1,11 +1,11 @@
 @echo off
-REM This script runs the Discord Game Chat Logger.
+REM This script runs the OSRS Clan Reporter
 
-echo Running Discord Game Chat Logger...
+echo Running OSRS Clan Reporter...
 echo.
 
 REM Execute the python script located in the src folder
-python src/main.py
+python regex_test.py
 
 echo.
 echo Script has finished.
